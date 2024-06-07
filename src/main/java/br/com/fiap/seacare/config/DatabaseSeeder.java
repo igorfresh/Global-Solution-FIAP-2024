09e94518d1a849e10f37e4a1cd89b7a2ac81bd2f
igorfresh <igormiguel37@gmail.com>
2024-06-07 19:22:57 -0300

    Add more info in DatabaseSeeder

--- src/main/java/br/com/fiap/seacare/config/DatabaseSeeder.java
+++ src/main/java/br/com/fiap/seacare/config/DatabaseSeeder.java
@@ -45,7 +45,37 @@ public class DatabaseSeeder implements CommandLineRunner{
                 .id(1L)
                 .name("plástico")
                 .description("pilha de garrafa pet")
-                .build()
+                .build(),
+                Artifact.builder()
+                    .id(2L)
+                    .name("metal")
+                    .description("latas de refrigerante amassada")
+                    .build(),
+                Artifact.builder()
+                    .id(3L)
+                    .name("papel")
+                    .description("jornais velho no mar")
+                    .build(),
+                Artifact.builder()
+                    .id(4L)
+                    .name("vidro")
+                    .description("garrafas de cerveja quebrada")
+                    .build(),
+                Artifact.builder()
+                    .id(5L)
+                    .name("pneu")
+                    .description("pneus velhos")
+                    .build(),
+                Artifact.builder()
+                    .id(6L)
+                    .name("plástico")
+                    .description("tampas de garrafa espalhadas")
+                    .build(),
+                    Artifact.builder()
+                        .id(7L)
+                        .name("tecido")
+                        .description("pedaços de roupa velha")
+                        .build()
             )
         );
 
@@ -55,7 +85,31 @@ public class DatabaseSeeder implements CommandLineRunner{
                 .id(1L).uf("SP")
                 .city("Santos")
                 .referencePoint("Praia do Gonzaga, próximo ao quiosque principal")
-                .build()
+                .build(),
+                Location.builder()
+                    .id(2L)
+                    .uf("RJ")
+                    .city("Rio de Janeiro")
+                    .referencePoint("Copacabana, perto do posto 6")
+                    .build(),
+                Location.builder()
+                    .id(3L)
+                    .uf("BA")
+                    .city("Salvador")
+                    .referencePoint("Praia do Porto da Barra, ao lado do farol")
+                    .build(),
+                Location.builder()
+                    .id(4L)
+                    .uf("SC")
+                    .city("Florianópolis")
+                    .referencePoint("Praia da Joaquina, próximo às dunas")
+                    .build(),
+                Location.builder()
+                    .id(5L)
+                    .uf("PE")
+                    .city("Recife")
+                    .referencePoint("Boa Viagem, em frente ao Parque Dona Lindu")
+                    .build()
             )
         );
 
@@ -65,7 +119,37 @@ public class DatabaseSeeder implements CommandLineRunner{
                 .id(1L)
                 .name("Igor Silva")
                 .phone("(11) 94815-1343")
-                .build()
+                .build(),
+                UserSC.builder()
+                    .id(2L)
+                    .name("Ana Souza")
+                    .phone("(21) 98765-4321")
+                    .build(),
+                UserSC.builder()
+                    .id(3L)
+                    .name("Carlos Oliveira")
+                    .phone("(31) 92345-6789")
+                    .build(),
+                UserSC.builder()
+                    .id(4L)
+                    .name("Mariana Lima")
+                    .phone("(41) 91234-5678")
+                    .build(),
+                UserSC.builder()
+                    .id(5L)
+                    .name("Rafael Costa")
+                    .phone("(51) 99876-5432")
+                    .build(),
+                UserSC.builder()
+                    .id(6L)
+                    .name("Fernanda Alves")
+                    .phone("(61) 98765-1234")
+                    .build(),
+                UserSC.builder()
+                    .id(7L)
+                    .name("Lucas Pereira")
+                    .phone("(71) 97654-3210")
+                    .build()
             )
         );
 
@@ -77,7 +161,49 @@ public class DatabaseSeeder implements CommandLineRunner{
                 .date(LocalDate.of(2018, 07, 22))
                 .userSc(userSCRepository.findById(1L).get())
                 .location(locationRepository.findById(1L).get())
-                .build()
+                .build(),
+                Report.builder()
+                    .id(2L)
+                    .description("Copacabana com muitos resíduos de alimentos e plásticos")
+                    .date(LocalDate.of(2019, 3, 15))
+                    .userSc(userSCRepository.findById(2L).get())
+                    .location(locationRepository.findById(2L).get())
+                    .build(),
+                Report.builder()
+                    .id(3L)
+                    .description("Praia do Porto da Barra com latas e garrafas espalhadas")
+                    .date(LocalDate.of(2020, 5, 10))
+                    .userSc(userSCRepository.findById(3L).get())
+                    .location(locationRepository.findById(3L).get())
+                    .build(),
+                Report.builder()
+                    .id(4L)
+                    .description("Praia da Joaquina cheia de pneus e pedaços de vidro")
+                    .date(LocalDate.of(2021, 1, 5))
+                    .userSc(userSCRepository.findById(4L).get())
+                    .location(locationRepository.findById(4L).get())
+                    .build(),
+                Report.builder()
+                    .id(5L)
+                    .description("Boa Viagem repleta de sacolas plásticas e restos de comida")
+                    .date(LocalDate.of(2019, 8, 21))
+                    .userSc(userSCRepository.findById(5L).get())
+                    .location(locationRepository.findById(5L).get())
+                    .build(),
+                Report.builder()
+                    .id(6L)
+                    .description("Praia do Gonzaga com grande quantidade de bitucas de cigarro")
+                    .date(LocalDate.of(2022, 11, 11))
+                    .userSc(userSCRepository.findById(6L).get())
+                    .location(locationRepository.findById(1L).get())
+                    .build(),
+                Report.builder()
+                    .id(7L)
+                    .description("Copacabana com entulhos de construção e plásticos")
+                    .date(LocalDate.of(2023, 4, 30))
+                    .userSc(userSCRepository.findById(7L).get())
+                    .location(locationRepository.findById(2L).get())
+                    .build()
             )
         );
 
@@ -87,7 +213,37 @@ public class DatabaseSeeder implements CommandLineRunner{
                 .id(1L)
                 .artifact(artifactRepository.findById(1L).get())
                 .report(reportRepository.findById(1L).get())
-                .build()
+                .build(),
+                ReportedArtifacts.builder()
+                    .id(2L)
+                    .artifact(artifactRepository.findById(2L).get()) 
+                    .report(reportRepository.findById(2L).get())  
+                    .build(),
+                ReportedArtifacts.builder()
+                    .id(3L)
+                    .artifact(artifactRepository.findById(3L).get())  
+                    .report(reportRepository.findById(3L).get())  
+                    .build(),
+                ReportedArtifacts.builder()
+                    .id(4L)
+                    .artifact(artifactRepository.findById(4L).get())  
+                    .report(reportRepository.findById(4L).get()) 
+                    .build(),
+                ReportedArtifacts.builder()
+                    .id(5L)
+                    .artifact(artifactRepository.findById(5L).get())  
+                    .report(reportRepository.findById(5L).get()) 
+                    .build(),
+                ReportedArtifacts.builder()
+                    .id(6L)
+                    .artifact(artifactRepository.findById(1L).get())  
+                    .report(reportRepository.findById(6L).get()) 
+                    .build(),
+                ReportedArtifacts.builder()
+                    .id(7L)
+                    .artifact(artifactRepository.findById(6L).get())  
+                    .report(reportRepository.findById(7L).get())  
+                    .build()
             )
         );
     }
