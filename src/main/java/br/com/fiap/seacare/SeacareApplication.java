c222999752e83ac42b18ba66cb275505a10b31bf
igorfresh <igormiguel37@gmail.com>
2024-06-07 20:18:53 -0300

    Add home page message

--- src/main/java/br/com/fiap/seacare/SeacareApplication.java
+++ src/main/java/br/com/fiap/seacare/SeacareApplication.java
@@ -2,12 +2,16 @@ package br.com.fiap.seacare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@Controller
 @OpenAPIDefinition(
 	info = @Info(
 		title = "API do SeaCare",
@@ -20,6 +24,12 @@ public class SeacareApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SeacareApplication.class, args);
+	}
+
+	@RequestMapping
+	@ResponseBody
+	public String home(){
+		return "SeaCare";
 	}
 
 }
